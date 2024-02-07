403a4b433ac2f2ef566645f1445f8db066569dc6
elinfelicia <elinriihimaki@gmail.com>
2024-02-07 13:39:15 +0100

    del-1 refactoring

--- del-1/main.js
+++ del-1/main.js
@@ -6,25 +6,25 @@ const submitBtn = document.querySelector(".submit-btn");
 const hexRes = document.querySelector(".hex-result");
 const rgbaRes = document.querySelector(".rgba-result");
 
-function Colour (r, g, b) {
+function Colour(r, g, b) {
     this.r = Math.max(0, Math.min(255, r));
     this.g = Math.max(0, Math.min(255, g));
     this.b = Math.max(0, Math.min(255, b));
 }
 
-Colour.prototype.rgb = function() {
+Colour.prototype.rgb = function () {
     return `rgb(${this.r},${this.g},${this.b})`;
 };
 
-Colour.prototype.hex = function() {
-        return "#" + ((1 << 24) + (this.r << 16) + (this.g << 8) + this.b).toString(16).slice(1);
-}
+Colour.prototype.hex = function () {
+    return "#" + ((1 << 24) + (this.r << 16) + (this.g << 8) + this.b).toString(16).slice(1);
+};
 
-Colour.prototype.rgba = function() {
+Colour.prototype.rgba = function () {
     return `rgba(${this.r},${this.g},${this.b},1)`;
-}
+};
 
-submitBtn.addEventListener("click", (event) => {
+function handleFormSubmit(event) {
     event.preventDefault();
     const r = parseInt(rInput.value);
     const g = parseInt(gInput.value);
@@ -32,10 +32,16 @@ submitBtn.addEventListener("click", (event) => {
     const bgColour = new Colour(r, g, b);
 
     wrapper.style.backgroundColor = bgColour.rgb();
-    hexRes.innerHTML = (`HEX: ${bgColour.hex()}`);
-    rgbaRes.innerHTML = (`RGBA: ${bgColour.rgba()}`);
+    hexRes.innerHTML = `HEX: ${bgColour.hex()}`;
+    rgbaRes.innerHTML = `RGBA: ${bgColour.rgba()}`;
 
+    clearInputFields();
+}
+
+function clearInputFields() {
     rInput.value = "";
     gInput.value = "";
     bInput.value = "";
-});
+}
+
+submitBtn.addEventListener("click", handleFormSubmit);
