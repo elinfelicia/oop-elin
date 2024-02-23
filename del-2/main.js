77f973463cc149fd69185d47e38ee4ed9442f0d0
elinfelicia <elinriihimaki@gmail.com>
2024-02-23 09:41:56 +0100

    first round logic

--- del-2/main.js
+++ del-2/main.js
@@ -6,11 +6,6 @@ class Card {
     }   
 }
 
-class Player {
-    constructor(){
-        this.playerName = playerName
-    }
-}
 class Deck {
     constructor() {
         this.cards = [];
@@ -27,7 +22,95 @@ class Deck {
             }
         }
     }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
+    }
+}
+console.log(new Deck());
+class Player {
+    constructor(name){
+        this.playerName = name;
+        this.playerCards = [];
+    }
+    getTotalValue() {
+        return this.playerCards.reduce((total, card) => total + card.value, 0);
+    }
+
+    sortHand() {
+        this.playerCards.sort((a, b) => b.value - a.value);
+    };
+    throwCards(throwPile) {
+        this.sortHand();
+        throwPile.push(this.playerCards.pop());
+        throwPile.push(this.playerCards.pop());
+    }
+
 }
 
-const deck = new Deck();
-console.log(deck.cards);
+class Game {
+    constructor() {
+        this.throwPile = [];
+        this.deck = new Deck(); 
+        this.players = [];
+    }
+    
+    start(playerOneName, playerTwoName) {
+        this.players.push(new Player(playerOneName));
+        this.players.push(new Player(playerTwoName));
+        
+        this.deck.shuffle();
+        
+        this.players[0].playerCards = this.deck.cards.slice(0, 5);
+        this.players[1].playerCards = this.deck.cards.slice(6, 10);
+
+        this.deck = new Deck();
+        this.deck.cards = this.deck.cards.filter(card => {
+            return !this.players.some(player => player.playerCards.some(playerCard => playerCard.name === card.name && playerCard.suit === card.suit));
+        });
+    }
+    
+    throwCards() {
+        this.players.forEach(player => {
+            player.throwCards(this.throwPile);
+        });
+    }
+}
+
+let newGame = new Game();
+
+newGame.start("Slim", "Luke");
+
+console.log("Slim's hand:");
+newGame.players[0].playerCards.forEach(card => {
+    console.log(`${card.name} of ${card.suit}`);
+});
+console.log(`Total value: ${newGame.players[0].getTotalValue()}`);
+
+console.log("\nLuke's hand:");
+newGame.players[1].playerCards.forEach(card => {
+    console.log(`${card.name} of ${card.suit}`);
+});
+console.log(`Total value: ${newGame.players[1].getTotalValue()}`);
+
+console.log("\nRemaining Deck:");
+newGame.deck.cards.forEach(card => {
+    console.log(`${card.name} of ${card.suit}`);
+});
+
+newGame.throwCards();
+
+console.log("\nSlim's hand:");
+newGame.players[0].playerCards.forEach(card => {
+    console.log(`${card.name} of ${card.suit}`);
+});
+console.log(`Total value: ${newGame.players[0].getTotalValue()}`);
+
+console.log("\nLuke's hand:");
+newGame.players[1].playerCards.forEach(card => {
+    console.log(`${card.name} of ${card.suit}`);
+});
+console.log(`Total value: ${newGame.players[1].getTotalValue()}`);
